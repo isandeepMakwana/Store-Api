2b7d7adf5f269f7fdf7009cadb4da9dc9e129d58
isandeepmakwana <isandeepmakwana1@gmail.com>
2022-11-28 18:26:23 +0530

    created a sample templete

--- 1-storefront/playground_app/views.py
+++ 1-storefront/playground_app/views.py
@@ -4,5 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+# def say_hello(request):
+#     return HttpResponse("hello world")
+
+
+
 def say_hello(request):
-    return HttpResponse("hello world")
+    return render(request, "hello.html",{'name':'sandeep'})
