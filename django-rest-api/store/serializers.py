c5d55e1adf74a0ed2c10f6350fd46ef0afc96e96
isandeepmakwana <isandeepmakwana1@gmail.com>
2022-12-17 20:58:57 +0530

    serializer 2 : - custom Serializer Fields created.

--- django-rest-api/store/serializers.py
+++ django-rest-api/store/serializers.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from rest_framework import serializers
 
 
@@ -8,4 +9,34 @@ class ProdcutSerializers(serializers.Serializer):
 
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    # unit_price = serializers.DecimalField(max_digits=6, decimal_places=2) isme model ke field se match hai
+    price = serializers.DecimalField(
+        max_digits=6, decimal_places=2, source="unit_price"
+    )  # isme model ke field se match nhi hai
+
+    # TODO creating custom Serializer Fields
+    # NOTE API Model (interface) !=Data Model(implementation)
+    price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
+
+    def calculate_tax(self, product):
+        return product.unit_price * Decimal(
+            1.1
+        )  # issue hota decimal not multiply to fload value
+
+    """
+    {
+        "id": 648,
+        "title": "7up Diet, 355 Ml",
+        "unit_price": 79.07,
+        "price_with_tax": 86.977
+    }
+    """
+    # and
+    """
+    {
+        "id": 648,
+        "title": "7up Diet, 355 Ml",
+        "price": 79.07,
+        "price_with_tax": 86.977
+    }
+    """
--- django-rest-api/store/views.py
+++ django-rest-api/store/views.py
@@ -3,14 +3,17 @@ from django.http import HttpResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Product
 from .serializers import ProdcutSerializers
 
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
-@api_view()
+"""@api_view()
 def product_list(request):
     # return HttpResponse("ok")
-    return Response("ok")
+    return Response("ok")"""
 
 
 # TODO creating Serializers
@@ -20,9 +23,38 @@ def product_list(request):
 - Serializer converts the product object to dict object
 - rest_framework have a jsocnrenderer class and this class have to render a method accept a dict object and returns json object."""
 
+# XXX issue if id =0 then show error
+# @api_view()
+# def product_detail(request, id):
+#     product = Product.objects.get(pk=id)
+#     serializer = ProdcutSerializers(product)
+#     return Response(serializer.data)
+
+# solv:
+
 
 @api_view()
 def product_detail(request, id):
-    product = Product.objects.get(pk=id)
+    # method 1:
+    # try:
+    #     product = Product.objects.get(pk=id)
+    #     serializer = ProdcutSerializers(product)
+    #     return Response(serializer.data)
+    # except Product.DoesNotExist:
+    #     return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    # method 2:
+    product = get_object_or_404(Product, pk=id)
     serializer = ProdcutSerializers(product)
+    return Response(serializer.data)
+
+    """{
+    "detail": "Not found."
+    }"""
+
+
+@api_view()
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProdcutSerializers(products, many=True)
     return Response(serializer.data)
