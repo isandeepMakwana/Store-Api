eea91e48c98b33a143f7139525c9ad1de863553e
isandeepmakwana <isandeepmakwana1@gmail.com>
2022-12-17 19:47:24 +0530

    serializer 1 : - basic one created

--- django-rest-api/store/views.py
+++ django-rest-api/store/views.py
@@ -3,20 +3,14 @@ from django.http import HttpResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Product
+from .serializers import ProdcutSerializers
 
 # Create your views here.
 @api_view()
 def product_list(request):
     # return HttpResponse("ok")
     return Response("ok")
-
-
-@api_view()
-def product_detail(request, id):
-    return Response(id)
-
-
-
 
 
 # TODO creating Serializers
@@ -27,3 +21,8 @@ def product_detail(request, id):
 - rest_framework have a jsocnrenderer class and this class have to render a method accept a dict object and returns json object."""
 
 
+@api_view()
+def product_detail(request, id):
+    product = Product.objects.get(pk=id)
+    serializer = ProdcutSerializers(product)
+    return Response(serializer.data)
